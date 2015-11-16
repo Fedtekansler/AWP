7668f5a905c440663c935df1e0d868e71be42209
Fedtekansler <peterkrogshede@Peters-MacBook-Pro.local>
2015-11-16 20:55:23 +0100

    Added TODO
    Still need to fix the movement.

--- Exercises 3/[3.1] The Fun Sorting Game/client.js
+++ Exercises 3/[3.1] The Fun Sorting Game/client.js
@@ -2,13 +2,21 @@
 var cols = document.querySelectorAll('.block .number-field');
 [].forEach.call(cols, function(col) {
     col.addEventListener('dragstart', dragstart, false);
+    col.addEventListener('dragleave', dragend, false);
 });
 
-var dropables = document.querySelectorAll('.block .dropable-area');
+var dropables = document.querySelectorAll('.block.active');
 [].forEach.call(dropables, function(dropable) {
     dropable.addEventListener('drop', drop, false);
     dropable.addEventListener('dragover', dragover, false);
 });
+
+// TODO SET CLASS TO BLOCK FOR DESTINATION TILE
+
+function dragend(event) {
+    // TODO SET CLASS TO BLOCK ACTIVE FOR MOVED TILE
+    event.className = "block active";
+}
 
 function dragover(event) {
     event.preventDefault();
