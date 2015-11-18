a9419b7ee1526cfcb9c2599e95d78813af9338e5
Fedtekansler <peterkrogshede@Peters-MacBook-Pro.local>
2015-11-18 20:31:41 +0100

    [3.2] Solved
    Should work like [3.1] now

--- Exercises 3/[3.2] JQuery Sorting Game/jquery.js
+++ Exercises 3/[3.2] JQuery Sorting Game/jquery.js
@@ -1,11 +1,9 @@
 $(function() {
+    setupTiles();
     handleDragAndDrop();
 });
 
-
-
 function handleDragAndDrop() {
-
     $(".block").draggable({
         revert: "invalid",
         helper: "clone",
@@ -13,6 +11,14 @@ function handleDragAndDrop() {
             if ($(".active").length == 0) {
                 $(this).find(".number-field").remove();
                 $(this).closest("div").addClass("active");
+                if (checkWinCondition()) {
+                    var gamecount = parseInt($("#gamecount").text());
+                    if (gamecount == 0) {
+                        localStorage.setItem("gamecount", 0);
+                    }
+                    localStorage.setItem("gamecount", parseInt(localStorage.getItem("gamecount")) + 1);
+                    resetTiles();
+                }
                 handleDragAndDrop();
             }
         }
@@ -28,4 +34,41 @@ function handleDragAndDrop() {
             $(this).droppable("destroy");
         }
     });
+}
+
+function checkWinCondition() {
+    var check = -1;
+    var boolean = true;
+    $(".block .number-field").each(function() {
+        var current = parseInt($(this).text());
+        if (check >= current) {
+            boolean = false;
+        }
+        check = current;
+    });
+    return boolean;
+}
+
+function setupTiles() {
+    var gamecount = localStorage.getItem("gamecount");
+    if (gamecount !== NaN) {
+        $("#gamecount").text(gamecount);
+    }
+    $(".block .number-field").each(function() {
+        var number = Math.floor((Math.random() * 100) + 1);
+        $(this).text(number);
+    });
+}
+
+function resetTiles() {
+    var last = $("#last");
+    var currentActive = $(".active");
+
+    if (last[0] !== currentActive[0]) {
+        currentActive.append(last[0].childNodes[3]);
+    }
+
+    last[0].className = "block edge active";
+    currentActive[0].className = "block";
+    setupTiles();
 }
