12e916fc7d2ad576839b6038294474362ba5bf88
Fedtekansler <peterkrogshede@Peters-MacBook-Pro.local>
2015-11-25 21:36:31 +0100

    [4.2] Added controller to directive

--- Exercises 4/[4.2] AngularJS Stars/script.js
+++ Exercises 4/[4.2] AngularJS Stars/script.js
@@ -1,15 +1,29 @@
 angular.module('starExample', [])
-.directive('rating', starRating);
+    .controller('ctrl', starControl)
+    .directive('rating', starRating);
+
+function starControl() {
+    this.rating = 5;
+}
 
 function starRating() {
     console.log("Hello World");
     return {
         templateUrl: 'partials/stars.html',
-        link: function(scope, element, attributes) {
+        controller: 'ctrl',
+        scope: {
+            max: '=?' // optional (default is 5)
+        },
+        link: function(scope, element, attributes, ctrl) {
+            if (scope.max == undefined) {
+                scope.max = 5;
+            }
+
             scope.stars = new Array(5);
 
             scope.toggle = function(index) {
                 console.log(index);
+                console.log(ctrl.rating);
             }
         }
     };
