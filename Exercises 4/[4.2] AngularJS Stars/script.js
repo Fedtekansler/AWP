a79fe04b9136a254b97144287c2ddbea908a7a33
Fedtekansler <peterkrogshede@Peters-MacBook-Pro.local>
2015-11-25 23:18:28 +0100

    [4.2] Solved: The star rating is continuously updated now.

--- Exercises 4/[4.2] AngularJS Stars/script.js
+++ Exercises 4/[4.2] AngularJS Stars/script.js
@@ -2,35 +2,40 @@ angular.module('starExample', [])
     .controller('ctrl', starControl)
     .directive('rating', starRating);
 
+// The controller defined for the body element of the module.
 function starControl() {
+    // The starting rating is set to 3.
     this.rating = 3;
 }
 
 function starRating() {
-    console.log("Hello World");
     return {
-        templateUrl: 'partials/stars.html',
-        controller: 'ctrl',
+        templateUrl: 'partials/stars.html', // A template is loaded with tags defined in link.
         scope: {
             ratingValue: '=ngModel',
-            max: '=?' // optional (default is 5)
+            max: '=?' // Max is set in the given template to 5
         },
-        link: function(scope, element, attributes, ctrl) {
-            if (scope.max == undefined) {
-                scope.max = 5;
+        link: function(scope, element, attributes) {
+            // We go through a loop up to max to add number of stars and color the stars depending on rating value.
+            function update() {
+                scope.stars = [];
+                for (var i = 0; i < scope.max; i++) {
+                    scope.stars.push({
+                        filled: i < scope.ratingValue
+                    });
+                }
             }
-
-            scope.stars = new Array(5);
 
             scope.toggle = function(index) {
-                //console.log(index);
-                //console.log(scope.ratingValue);
+                // This function is attached as an onclick, where add one to make up for the index.
                 scope.ratingValue = index + 1;
-            }
+            };
 
             scope.$watch('ratingValue', function(newValue, oldValue) {
-                console.log("Old value is " + oldValue);
-                console.log("New value is " + newValue);
+                // This is used for callback. We just simply update the view on any change to the value.
+                if (newValue) {
+                    update();
+                }
             });
         }
     };
