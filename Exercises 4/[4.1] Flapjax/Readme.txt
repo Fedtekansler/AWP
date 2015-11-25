1adfba907aaf331faad8ccb391b89689f7bce669
Fedtekansler <peterkrogshede@Peters-MacBook-Pro.local>
2015-11-25 18:22:31 +0100

    [4.2] Setting up the stars...

---
+++ Exercises 4/[4.1] Flapjax/Readme.txt
@@ -0,0 +1,1 @@
+See Flapjax.pdf.
--- Exercises 4/[4.2] AngularJS Stars/script.js
+++ Exercises 4/[4.2] AngularJS Stars/script.js
@@ -1,23 +1,9 @@
-/**
- * Created by Victor on 24-11-2015.
- */
+angular.module('starExample', [])
+.directive('rating', starRating);
 
-angular.module('components', [])
-    .directive('rating', function(){
-        return{
-            restrict: 'E',
-            scope:{
-                //why can't i bind my stars?
-               stars:'bind'
-            },
-            templateUrl: 'partials/Stars.html'
-        }
-    });
-
-angular.module('StarRating', ['components'])
-
-function StarCtrl($scope){
-    $scope.totalStars = function(){
-
+function starRating() {
+    console.log("Hello World");
+    return {
+        templateUrl: 'partials/stars.html'
     };
 }
