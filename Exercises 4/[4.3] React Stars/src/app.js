87712dc820d3b3bd6080fd18d470e21152062517
chrisgulddahl <chrisgpedersen@gmail.com>
2015-11-27 11:41:06 +0100

    Exercise 4.3. Displaying stars works.

--- Exercises 4/[4.3] React Stars/src/app.js
+++ Exercises 4/[4.3] React Stars/src/app.js
@@ -1,20 +1,31 @@
-var Hello = React.createClass({
-  getInitialState: function() {
-    return {name: ''};
-  },
-  handleChange: function(event) {
-    this.setState({name: event.target.value});
-  },
-  render: function() {
-    return <div>
-	  Please enter your name here: <input value={this.state.name} onChange={this.handleChange}/>
-      <p>Hello {this.state.name || 'stranger'}!</p>
-	</div>;
-  }
+var Rating = React.createClass({
+	getInitialState: function() {
+		return {stars: 0};
+	},
+	handleChange: function(event) {
+		//this.setState({name: event.target.value});
+	},
+	render: function() {
+		var starElements = [];
+		var currentRating = this.state.stars || this.props.stars
+		for (var i = 1; i <= this.props.max; i++) {
+			if(i <= currentRating){
+				starElements.push(<span>&#9733;</span>);
+			}
+			else{
+				starElements.push(<span>&#9734;</span>);
+			}
+		}
+		return <div>{starElements}</div>;
+		/*return <div>
+			Please enter your name here: <input value={this.state.name} onChange={this.handleChange}/>
+			<p>Hello {this.state.name || 'stranger'}!</p>
+		</div>;*/
+	}
 });
 
 ReactDOM.render(
-  <Hello/>,
+  <Rating stars="3" max="5" />,
   document.getElementById("container")
 );
 
