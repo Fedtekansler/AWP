5e9e4d516fb2ce1ff4b375a8ef6b08b29daf43fd
Fedtekansler <peterkrogshede@Peters-MacBook-Pro.local>
2015-11-29 16:57:10 +0100

    [4.4] Added http loading from JSON
    Den virker ikke sammen med vores egen directive rating endnu.

---
+++ Exercises 4/[4.4] Movie Ratings/AngularJS/script.js
@@ -0,0 +1,44 @@
+angular.module('starExample', [])
+    .controller('ctrl', starControl)
+    .directive('rating', starRating);
+
+// The controller defined for the body element of the module.
+function starControl($scope, $http) {
+    // The starting rating is set to 3.
+    $http.get("movies.json").then(function(response) {
+        $scope.movies = response.data.movies;
+    });
+}
+
+function starRating() {
+    return {
+        templateUrl: 'template/stars.html', // A template is loaded with tags defined in link.
+        scope: {
+            ratingValue: '=ngModel',
+            max: '=?' // Max is set in the given template to 5
+        },
+        link: function(scope, element, attributes) {
+            // We go through a loop up to max to add number of stars and color the stars depending on rating value.
+            function update() {
+                scope.stars = [];
+                for (var i = 0; i < scope.max; i++) {
+                    scope.stars.push({
+                        filled: i < scope.ratingValue
+                    });
+                }
+            }
+
+            scope.toggle = function(index) {
+                // This function is attached as an onclick, where we add one to make up for the index.
+                scope.ratingValue = index + 1;
+            };
+
+            scope.$watch('ratingValue', function(newValue, oldValue) {
+                // This is used for callback. We just simply update the view on any change to the value.
+                if (newValue) {
+                    update();
+                }
+            });
+        }
+    };
+}
