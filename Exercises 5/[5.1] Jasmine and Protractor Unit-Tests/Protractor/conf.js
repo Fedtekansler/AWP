3d03f54196baef6cfeeb9ed39de1775974ef2b84
chrisgulddahl <chrisgpedersen@gmail.com>
2015-12-08 20:45:05 +0100

    Protractor tests still work..

--- Exercises 5/[5.1] Jasmine and Protractor Unit-Tests/Protractor/conf.js
+++ Exercises 5/[5.1] Jasmine and Protractor Unit-Tests/Protractor/conf.js
@@ -1,5 +1,6 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.js']
+    specs: ['spec.js'],
+	baseUrl: 'http://localhost/AWP/Exercises%205/%5b5.1%5d%20Jasmine%20and%20Protractor%20Unit-Tests/Protractor/app/'
 }
--- Exercises 5/[5.1] Jasmine and Protractor Unit-Tests/Protractor/spec.js
+++ Exercises 5/[5.1] Jasmine and Protractor Unit-Tests/Protractor/spec.js
@@ -1,9 +1,7 @@
-var baseUrl = 'http://localhost/AWP/Exercises%205/%5b5.1%5d%20Jasmine%20and%20Protractor%20Unit-Tests/Protractor/app/';
-
 describe('Row Manager App', function() {
 	browser.ignoreSynchronization = true; // Don't wait for angular to do it's thing..
     it('should have a title', function() {
-        browser.get(baseUrl+'index.html');
+        browser.get('index.html');
 
         expect(browser.getTitle()).toEqual('jQuery Row Manager');
     });
