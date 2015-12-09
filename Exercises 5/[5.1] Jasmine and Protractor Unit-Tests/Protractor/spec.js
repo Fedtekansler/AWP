e6226a9f911beda84dafe8dcac2aed71d5b5c21a
chrisgulddahl <chrisgpedersen@gmail.com>
2015-12-09 12:47:47 +0100

    Protractor tests done.

--- Exercises 5/[5.1] Jasmine and Protractor Unit-Tests/Protractor/spec.js
+++ Exercises 5/[5.1] Jasmine and Protractor Unit-Tests/Protractor/spec.js
@@ -15,10 +15,27 @@ describe('Angular Star Rating', function() {
 		browser.get('index.html');
 		
 		// Click 2nd star
-		element.all(by.repeater('star in stars track by $index')).get(1).click();
+		element.all(by.repeater('star in stars track by $index')).get(2-1).click();
 		
 		// Rating should be 2 now
 		var rating = element(by.css('p'));
 		expect(rating.getText()).toEqual("Rating: 2");
+	});
+	
+	it('should change star colors when rating changes', function(){
+		browser.get('index.html');
+		
+		var stars = element.all(by.repeater('star in stars track by $index'));
+		
+		// Click 5th star
+		stars.get(5-1).click();
+		
+		// 5 stars should be full/yellow now
+		var yellowStars = stars.filter(function(star){
+			return star.getAttribute('class').then(function(classes){
+				return classes.indexOf('filled') > -1;
+			});
+		});
+		expect(yellowStars.count()).toEqual(5);
 	});
 });
