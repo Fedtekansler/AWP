57d0eed853e855634d23b71a064e75c3ba5da70b
chrisgulddahl <chrisgpedersen@gmail.com>
2015-12-09 12:22:51 +0100

    Added another protractor test.

--- Exercises 5/[5.1] Jasmine and Protractor Unit-Tests/Protractor/spec.js
+++ Exercises 5/[5.1] Jasmine and Protractor Unit-Tests/Protractor/spec.js
@@ -7,7 +7,18 @@ describe('Angular Star Rating', function() {
 	
 	it('should have default rating 3', function(){
 		browser.get('index.html');
-		var rating = element.all(by.css('p')).first();
+		var rating = element(by.css('p'));
 		expect(rating.getText()).toEqual("Rating: 3");
+	});
+	
+	it('should change rating when a star is clicked', function(){
+		browser.get('index.html');
+		
+		// Click 2nd star
+		element.all(by.repeater('star in stars track by $index')).get(1).click();
+		
+		// Rating should be 2 now
+		var rating = element(by.css('p'));
+		expect(rating.getText()).toEqual("Rating: 2");
 	});
 });
